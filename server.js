971b3c7b8a64ae1ce9774c6e5929b40a9f1f1512
euerickraphael <ersa2@aluno.ifal.edu.br>
2023-07-19 17:34:48 -0300

    2.0.1.5

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function DeleteDados(req, resp) {
 };
 
 function Index(req, resp) {
-    resp.render("pages/index.ejs");
+    resp.sendFile(__dirname+"/views/index.html");
 };
 
 server.get("/", Index);
